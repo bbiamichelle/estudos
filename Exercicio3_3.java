c5cd7a87bbd90af1dc5335009da06adeadfb0b41
biamichelle <biamichelle@usp.br>
2017-11-28 17:57:48 -0200

    terceiro commit

---
+++ Exercicio3_3.java
@@ -0,0 +1,10 @@
+public class Exercicio3_3 {
+
+    public static void main(String[] args) {
+        for (int i =0; i <=100; i++){
+            if (i%3 == 0){
+                System.out.println(i + " é múltiplo de 3");
+            }
+        }
+    }
+}
---
+++ Exercicio3_4.java
@@ -0,0 +1,14 @@
+public class Exercicio3_4 {
+
+    public static void main(String[] args) {
+
+        int fatorial = 1;
+        for (int n = 1; n <= 10; n++ ){
+
+                fatorial = fatorial * n;
+
+            System.out.println("Fatorial de " + n + "=" + fatorial);
+        }
+
+    }
+}
